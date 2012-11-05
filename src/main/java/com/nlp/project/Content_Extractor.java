4f63e9598af2fa690ed86db3933b4d7a7b66af0f
Nilesh Poddar <nilesh0489@gmail.com>
2012-11-05 12:07:30 -0500

    Done with extraction of metadata and the content from url

---
+++ src/main/java/com/nlp/project/Content_Extractor.java
@@ -0,0 +1,43 @@
+package com.nlp.project;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import com.gravity.goose.Article;
+import com.gravity.goose.Configuration;
+import com.gravity.goose.Goose;
+
+public class Content_Extractor {
+	
+	public Content_Extractor() {
+		
+	}
+	
+	public void extract(String filename, String mode) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(filename));
+		int file_number = 1;
+		String s;		
+		while ((s = br.readLine()) != null){
+			PrintWriter out = new PrintWriter(new File(mode + "/" +  file_number + ".txt"));
+			Goose goose = new Goose(new Configuration());
+			try {				
+				Article article = goose.extractContent(s);
+				out.println(article.cleanedArticleText());
+			} catch (Exception e) {
+				//Bypass the error
+			}
+			out.close();
+			file_number++;
+		}
+		br.close();
+	}
+	
+	public static void main(String args[]) throws IOException {
+		Content_Extractor ce = new Content_Extractor();
+		ce.extract("url-top-150.txt", "top");
+		ce.extract("url-bottom-150.txt", "bottom");
+	}
+}
