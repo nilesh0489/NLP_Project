bc57a5873745e8665692fb28c5fb8ad5c8067ec4
Nilesh Poddar <nilesh0489@gmail.com>
2012-11-19 19:47:20 -0500

    Updated the code to ignore the not applicable domains

--- src/main/java/com/nlp/project/MetaData.java
+++ src/main/java/com/nlp/project/MetaData.java
@@ -1,3 +1,4 @@
+package com.nlp.project;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -6,40 +7,61 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
 
-
-
-public class Metadata {
-	public static void main(String args[]) {
+public class MetaData {
+	
+	public MetaData() {
+		
+	}
+	
+	public void extractMetadata(String filename, String mode) {
 		try {
-				BufferedReader fp = new BufferedReader(new FileReader("E:\\nlp_proj_data\\metadata-bottom-tech-reddit-week-400.txt"));
-				PrintWriter out = new PrintWriter(new File("E:\\nlp_proj_data\\filtered-metadata-bottom-400.txt"));
-				String s;
-				int i=1;
-				while ((s = fp.readLine()) != null){
-					String[] posts = s.split("}},");
-					for (String post : posts){
-						String[] fields = post.split(", \"");
-							out.println(i++ + ". ");
-						for( String field : fields) {
-							if (field.startsWith("score") || field.startsWith("title") || field.startsWith("created") || field.startsWith("created_utc") || field.startsWith("data\": {\"domain\":") || field.startsWith("permalink") || field.startsWith("url") )
-								if (field.startsWith("data\": {\"domain\": \"youtube.com"))
-									out.println("DOMAIN NOT APPLICABLE\n");
-								else if (field.startsWith("created") || field.startsWith("created_utc")) {
-									String[] field_parts = field.split(" ");
-									String[] field_subparts = field_parts[1].split("\\.");
-									long dateLong = Long.parseLong(field_subparts[0]);
-									dateLong = dateLong * 1000;
-									String date = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new Date(dateLong));
-									out.println(field_parts[0].replaceAll("\":", ": ") + date ); 
+			BufferedReader fp = new BufferedReader(new FileReader(filename));
+			PrintWriter out = new PrintWriter(new File("tech-400/filtered-data-" + mode + "-400.txt"));
+			PrintWriter out1 = new PrintWriter(new File("tech-400/url-" + mode + "-400.txt"));
+			String s;
+			boolean flag;
+			while ((s = fp.readLine()) != null){
+				String[] posts = s.split("}},");
+				for (String post : posts){
+					flag = false;
+					String[] fields = post.split(", \"");
+					for( String field : fields) {
+						if (field.startsWith("score") || field.startsWith("title") || field.startsWith("created") || field.startsWith("created_utc") || field.startsWith("data\": {\"domain\":") || field.startsWith("permalink") || field.startsWith("url") ) {
+							if (field.startsWith("data\": {\"domain\": \"youtube.com")) {
+								flag = true;
+								break;
+							}									
+							else if (field.startsWith("created") || field.startsWith("created_utc")) {
+								String[] field_parts = field.split(" ");
+								String[] field_subparts = field_parts[1].split("\\.");
+								long dateLong = Long.parseLong(field_subparts[0]);
+								dateLong = dateLong * 1000;
+								String date = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new Date(dateLong));
+								out.println(field_parts[0].replaceAll("\":", ": ") + date ); 
+							}
+								else {
+									if (field.startsWith("url")) {
+										field.replaceAll("data\": \\{\"", "").replaceAll("\": ", ": ");											
+										field = field.replace("\"", "");
+										field = field.replace("url: ", "");
+										out1.println(field);											
+									}
+									else {
+										out.println(field.replaceAll("data\": \\{\"", "").replaceAll("\": ", ": "));
+									}
+									
 								}
-								else
-									out.println(field.replaceAll("data\": \\{\"", "").replaceAll("\": ", ": "));
 						}
+					}
+					if(!flag) {
 						out.println("\n");
 					}
+											
 				}
-				fp.close();
-				out.close();
+			}
+			fp.close();
+			out.close();
+			out1.close();
 		}
 		catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -47,6 +69,12 @@ public class Metadata {
 		catch (IOException e) {
 			e.printStackTrace();
 		} 
+	}
+	
+	public static void main(String args[]) {
+		MetaData md = new MetaData();
+		md.extractMetadata("tech-400/metadata-bottom-tech-reddit-week-400.txt", "bottom");
+		md.extractMetadata("tech-400/metadata-top-tech-reddit-week-400.txt", "top");
 	}
 
 }
